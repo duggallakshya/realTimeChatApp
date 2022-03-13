d0118796581f242e94267f512f6927d6a40c472e
lakshya123 <duggallakshya@gmail.com>
2022-03-14 02:03:55 +0530

    initial commit

---
+++ public/client.js
@@ -0,0 +1,53 @@
+const socket = io()
+let name;
+let textarea = document.querySelector("#textarea");
+let messageArea = document.querySelector(".message_area");
+do {
+   name = prompt("Please enter your name: ")
+}while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(msg) {
+    let msgObj = {
+        user: name,
+        message:msg.trim()
+    }
+
+    appendMessage(msgObj, 'outgoing');
+    textarea.value = "";
+    scrollToBottom();
+
+    socket.emit('message' , msgObj)
+
+    
+}
+
+function appendMessage(msgObj, type){
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msgObj.user}</h4>
+        <p>${msgObj.message}</p>
+    `
+
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
+}
+
+//Receive msg
+
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming');
+    scrollToBottom();
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const http = require('http').createServer(app);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",(req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+http.listen(PORT , () => {
+    console.log(`listening on ${PORT}`);
+})
+
+// Socket Confirguration
+
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log("connected to socket");
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);
+    }) 
+})
